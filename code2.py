add50affc579b2bf0e5cce279b08c5f1ef646b29
Anurag Kandulna <dev.ak@outlook.in>
2024-04-12 17:19:04 +0530

    new file

---
+++ code2.py
@@ -0,0 +1,29 @@
+# str1 = "abc"
+# str2 = "aab"
+
+# formattedText1 = ""
+#
+# with open('Json1.rtf', 'r') as f:
+#     text1 = f.read()
+#     formattedText = text1.encode("utf-8")
+#     print(formattedText)
+#     formattedText1 = formattedText
+#
+#
+# jsonObject = json.loads(formattedText1)
+# # print(jsonObject)
+#
+# key = "dataLogMaxSize"
+# key1 = "web-app"
+#
+# text2 = deepcopy(jsonObject)
+#
+# findKeyAndReplaceValue(keyToFind=key, valueToReplace="abc", jsonObj=text2, ctr=1)
+#
+#
+# print(jsonObject == text2)
+# print(text2)
+
+# print(jsonObject)
+# l1 = ['123', '345']
+# print(l1.keys())
