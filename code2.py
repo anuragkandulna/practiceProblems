e5294ae93fdf1412a829d1de9b87abe583f51666
Anurag Kandulna <dev.ak@outlook.in>
2024-04-12 17:42:53 +0530

    solution for problem 2

--- code2.py
+++ code2.py
@@ -1,29 +1,34 @@
-# str1 = "abc"
-# str2 = "aab"
+# PROBLEM STATEMENT 2:
+# Given a string verify that a permutation of the characters is a palindrome or not.
+# Tc 1: input = "abc" | output = False
+# Tc 2: input = "aba" | output = True
+# Tc 3: input = "aa" | output = True
+# Tc 4: input = "" | output = print error [Here assumption is that null strings are invalid inputs]
 
-# formattedText1 = ""
-#
-# with open('Json1.rtf', 'r') as f:
-#     text1 = f.read()
-#     formattedText = text1.encode("utf-8")
-#     print(formattedText)
-#     formattedText1 = formattedText
-#
-#
-# jsonObject = json.loads(formattedText1)
-# # print(jsonObject)
-#
-# key = "dataLogMaxSize"
-# key1 = "web-app"
-#
-# text2 = deepcopy(jsonObject)
-#
-# findKeyAndReplaceValue(keyToFind=key, valueToReplace="abc", jsonObj=text2, ctr=1)
-#
-#
-# print(jsonObject == text2)
-# print(text2)
 
-# print(jsonObject)
-# l1 = ['123', '345']
-# print(l1.keys())
+def isPalindrome(inputStr):
+    """Return True/False if valid palindrome string. Else return None."""
+
+    if len(inputStr) <= 0:
+        print('Null string given as input.')
+        return
+
+    hashMap = {}
+    for c in inputStr:
+        hashMap[c] = hashMap.get(c, 0) + 1
+
+    # Logic is that if the string is palindrome then the number of characters with odd numbers is either 0 or 1.
+    # i.e, the middle index can have only 1 char if length is odd, but in case of odd its 0
+    oddCharCount = 0
+    for k, v in hashMap.items():
+        if v % 2 != 0:
+            oddCharCount += 1
+
+    return oddCharCount <= 1
+
+
+# Test cases
+testCases = ["abc", "aba", "aa", ""]
+
+for s in testCases:
+    print(isPalindrome(inputStr=s))
